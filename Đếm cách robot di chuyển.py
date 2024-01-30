9eda9e5f5eab396134e9c29a040059f7b0d38e1b
CKLCDer1412 <118926856+CKLCDer1412@users.noreply.github.com>
2024-01-30 23:34:10 +0700

    Update Đếm cách robot di chuyển.py

--- Đếm cách robot di chuyển.py
+++ Đếm cách robot di chuyển.py
@@ -1,17 +1,17 @@
-def uniquePathWith0bstacles(obstacleGrid):
+def uniquePath(obs):
     def ways(i , j):
-        if i == cols and j == rows and obstacleGrid[j][i] == 0:
+        if i == cols and j == rows and obs[j][i] == 0:
             return 1
-        if i > cols or j > rows or  obstacleGrid[j][i] == 1:
+        if i > cols or j > rows or  obs[j][i] == 1:
             return 0
         if i not in res and j not in res:
             res[i, j] = ways(i+1, j) + ways(i, j+1)
         return res[i, j]
     res={}
-    cols, rows = len(obstacleGrid[0])-1, len(obstacleGrid)-1
+    cols, rows = len(obs[0])-1, len(obs)-1
     return ways(0, 0)
 
-obstacleGrid = [[0,0,0],
+obs = [[0,0,0],
                 [0,0,0],
                 [0,0,0]]
-print(uniquePathWith0bstacles(obstacleGrid))
+print(uniquePath(obs))
